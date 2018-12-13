01aaf3b0e275a27c4ceb4956d062d84c6964fd0b
Amit Dhamu <dev@amitd.co>
2018-12-13 15:31:06 +0000

    Code maintenance

--- index.php
+++ index.php
@@ -2,17 +2,14 @@
     require __DIR__ . '/vendor/autoload.php';
     $app = (new Status\Container)->get();
 
-    echo $app->getTwigEnvironment()->render(
-        'pages/index.html.twig',
-        [
-            'base' => 'http://' . $_SERVER['HTTP_HOST'] . '/',
-            'stylesheet' => $app->getStylesheetFilename(),
-            'script' => $app->getScriptFilename(),
-            'currentYear' => date("Y"),
-            'timestamp' => date("D j M Y H:i:s e"),
-            'statuses' => $app->getStatuses(),
-            'uptime' => $app->getSystemCheckerService()->getUptime(),
-            'machineName' => $app->getSystemCheckerService()->getMachineName(),
-            'ip' => $app->getSystemCheckerService()->getIP()
-        ]
-    );
+    echo $app->getTwigEnvironment()->render('pages/index.html.twig', [
+        'base' => 'http://' . $_SERVER['HTTP_HOST'] . '/',
+        'stylesheet' => $app->getStylesheetFilename(),
+        'script' => $app->getScriptFilename(),
+        'currentYear' => date("Y"),
+        'timestamp' => date("D j M Y H:i:s e"),
+        'statuses' => $app->getStatuses(),
+        'uptime' => $app->getSystemCheckerService()->getUptime(),
+        'machineName' => $app->getSystemCheckerService()->getMachineName(),
+        'ip' => $app->getSystemCheckerService()->getIP()
+    ]);
